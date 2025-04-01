7a91d102b95d31074e45dcadf6a81030ee5f007f
IoanaO98 <osan.ioana1998@gmail.com>
2025-04-01 13:35:41 +0300

    some design added

--- ioana-portfolio/src/app/main-content/main-content.component.ts
+++ ioana-portfolio/src/app/main-content/main-content.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
 export class MainContentComponent {
+  title = 'Full Stack Developer.';
+  titleLetters = this.title.split('');
 
+  skills = [
+    'Typescript',
+    'Javascript',
+    'Angular',
+    '.Net',
+    'Bootstrap',
+    'EntityFramework',
+    'Azure',
+    'Tailwind',
+    'C#',
+    'HTML',
+    'CSS',
+    'GitHub',
+  ].map((skill) => ({
+    name: skill,
+    style: {
+      top: `${Math.random() * 80 + 5}%`,
+      left: `${Math.random() * 80 + 5}%`,
+      animationDelay: `${Math.random() * 2}s`,
+      fontSize: `${Math.random() * 20 + 14}px`,
+    },
+  }));
 }
