fef0515a3ad70e449f94e26dff2bab86b67fea2a
IoanaO98 <osan.ioana1998@gmail.com>
2025-04-01 13:51:32 +0300

    fixes on animation

--- ioana-portfolio/src/app/main-content/main-content.component.ts
+++ ioana-portfolio/src/app/main-content/main-content.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
+  showTitle = false;
   title = 'Full Stack Developer.';
   titleLetters = this.title.split('');
 
@@ -25,13 +26,26 @@ export class MainContentComponent {
     'HTML',
     'CSS',
     'GitHub',
-  ].map((skill) => ({
-    name: skill,
-    style: {
-      top: `${Math.random() * 80 + 5}%`,
-      left: `${Math.random() * 80 + 5}%`,
-      animationDelay: `${Math.random() * 2}s`,
-      fontSize: `${Math.random() * 20 + 14}px`,
-    },
-  }));
+  ].map((skill, index) => {
+    const delayIn = index * 0.3;
+    const delayOut = delayIn + 1.5;
+
+    return {
+      name: skill,
+      style: {
+        top: `${Math.random() * 80 + 5}%`,
+        left: `${Math.random() * 80 + 5}%`,
+        fontSize: `${Math.random() * 20 + 14}px`,
+        '--delay-in': `${delayIn}s`,
+        '--delay-out': `${delayOut}s`,
+      },
+    };
+  });
+  ngOnInit(): void {
+    const totalDelay = this.skills.length * 0.3 + 2.5;
+
+    setTimeout(() => {
+      this.showTitle = true;
+    }, totalDelay * 1000);
+  }
 }
