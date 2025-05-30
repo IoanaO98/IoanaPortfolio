f3d33193997fd83a7e4b15bba56071c9dce5130d
IoanaO98 <osan.ioana1998@gmail.com>
2025-05-30 22:39:55 +0300

    some fixes

--- ioana-portfolio/src/app/services-section/services-section.component.ts
+++ ioana-portfolio/src/app/services-section/services-section.component.ts
@@ -1,33 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
+import {
+  CarouselComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  CarouselControlComponent,
+} from '@coreui/angular';
 
 @Component({
-    selector: 'app-services-section',
-    imports: [SlickCarouselModule, CommonModule],
-    templateUrl: './services-section.component.html',
-    styleUrl: './services-section.component.scss'
+  selector: 'app-services-section',
+  imports: [
+    CommonModule,
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    CarouselControlComponent,
+  ],
+  templateUrl: './services-section.component.html',
+  styleUrl: './services-section.component.scss',
 })
 export class ServicesSectionComponent {
+  activeIndex = 0;
+  visibleCount = 3;
+
   cards = [
     {
+      id: 1,
       title: 'UI/UX',
       description: 'Creating intuitive and visually appealing designs',
     },
-    { title: 'Web Design', description: 'Responsive and engaging websites' },
-    { title: 'App Design', description: 'Mobile-friendly interfaces' },
-    { title: 'Prototyping', description: 'Interactive design flows' },
+    {
+      id: 2,
+      title: 'Web Design',
+      description: 'Responsive and engaging websites',
+    },
+    { id: 3, title: 'App Design', description: 'Mobile-friendly interfaces' },
+    { id: 4, title: 'Prototyping', description: 'Interactive design flows' },
+    { id: 5, title: 'Wireframing', description: 'Design system creation' },
   ];
 
-  slideConfig = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: true,
-    infinite: true,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 2 } },
-      { breakpoint: 768, settings: { slidesToShow: 1 } },
-    ],
-  };
+  nextSlide() {
+    this.activeIndex = (this.activeIndex + 1) % this.cards.length;
+  }
+
+  prevSlide() {
+    this.activeIndex =
+      (this.activeIndex - 1 + this.cards.length) % this.cards.length;
+  }
+
+  goToSlide(index: number) {
+    this.activeIndex = index;
+  }
 }
